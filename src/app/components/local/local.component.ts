66b9af59e8798fd5a5a8d87a2732b425f3f975db
Eduardo Weise <eduardo.weise@ndd.tech>
2026-02-06 16:43:49 -0300

    fix: correção da localização do evento

--- src/app/components/local/local.component.ts
+++ src/app/components/local/local.component.ts
@@ -17,18 +17,18 @@ export class LocalComponent {
   city = 'Santa Rosa - RS';
   cep = '98900-000';
   
-  latitude = -27.8688656;
-  longitude = -54.4284968;
+  latitude = -27.8687971;
+  longitude = -54.4259527;
   
   mapUrl: SafeResourceUrl;
   
   constructor(private readonly sanitizer: DomSanitizer) {
-    const googleMapsEmbedUrl = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.0!2d${this.longitude}!3d${this.latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM!5e0!3m2!1spt-BR!2sbr!4v1234567890123!5m2!1spt-BR!2sbr`;
+    const googleMapsEmbedUrl = `https://maps.google.com/maps?q=Morada+dos+Ventos,+Santa+Rosa+-+RS&z=16&output=embed&hl=pt-BR`;
     this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(googleMapsEmbedUrl);
   }
   
   openInMaps(): void {
-    const mapsUrl = `https://www.google.com/maps/place/Morada+dos+Ventos/@${this.latitude},${this.longitude}z/data=!3m1!4b1!4m6!3m5!1s0x94f94b0069b10bd5:0xa5924cc1056f6d14!8m2!3d-27.8688656!4d-54.4259219!16s%2Fg%2F11lmdw0tnm?entry=ttu&g_ep=EgoyMDI2MDIwMS4wIKXMDSoASAFQAw%3D%3D`;
+    const mapsUrl = `https://www.google.com/maps/place/Morada+dos+Ventos/@${this.latitude},${this.longitude}z/data=!4m14!1m7!3m6!1s0x94f94b0069b10bd5:0xa5924cc1056f6d14!2sMorada+dos+Ventos!8m2!3d-27.8688656!4d-54.4259219!16s%2Fg%2F11lmdw0tnm!3m5!1s0x94f94b0069b10bd5:0xa5924cc1056f6d14!8m2!3d-27.8688656!4d-54.4259219!16s%2Fg%2F11lmdw0tnm?entry=ttu&g_ep=EgoyMDI2MDIwMy4wIKXMDSoASAFQAw%3D%3D`;
     window.open(mapsUrl, '_blank');
   }
 }
