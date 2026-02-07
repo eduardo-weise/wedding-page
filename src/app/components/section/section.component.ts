ffde3747070cb6d9760bc0d94aaaafe031722fa9
Eduardo Weise <eduardo.weise@ndd.tech>
2026-02-07 15:01:36 -0300

    feat: adicionado componente genérico de seções

---
+++ src/app/components/section/section.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ScrollRevealDirective } from '../../directives/scroll-reveal.directive';
+import { ScrollActiveDirective } from '../../directives/scroll-active.directive';
+
+@Component({
+  selector: 'app-section',
+  standalone: true,
+  imports: [CommonModule, ScrollRevealDirective, ScrollActiveDirective],
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.scss']
+})
+export class SectionComponent {
+  @Input() sectionId = '';
+  @Input() sectionNumber = '';
+  @Input() sectionLabel = '';
+  @Input() title = '';
+  @Input() subtitle = '';
+}
