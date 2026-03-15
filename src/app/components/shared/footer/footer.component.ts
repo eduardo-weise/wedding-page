556379fbab88439c59fc4970b8fc24e16fbb4bec
Eduardo Weise <eduardo.weise@ndd.tech>
2026-03-15 16:59:06 -0300

    chore: aplicar animções ao footer

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,10 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, AfterViewInit } from '@angular/core';
+import { animate, onScroll } from 'animejs';
 
 @Component({
-  selector: 'app-footer',
-  standalone: true,
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'app-footer',
+	standalone: true,
+	templateUrl: './footer.component.html',
+	styleUrl: './footer.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {}
+export class FooterComponent implements AfterViewInit {
+
+	ngAfterViewInit(): void {
+		const anim = animate('.footer-logo', {
+			y: ['100%', '0%'],
+			opacity: [0, 1],
+			duration: 800,
+			ease: 'outCubic',
+			autoplay: false,
+		});
+
+		onScroll({
+			target: '.footer-logo',
+			enter: 'bottom',
+			leave: 'bottom',
+			onEnter: () => {
+				anim.reversed = false;
+				anim.play();
+			},
+			onLeave: () => {
+				anim.reversed = true;
+				anim.refresh();
+			},
+		});
+	}
+}
